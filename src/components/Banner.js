96ae5e8668974f16ff54f854501b1743588b086f
w0501872 <w0501872@campus.nscc.ca>
2024-04-15 20:10:43 -0300

    made few more changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -70,7 +70,8 @@ export const Banner = () => {
           <Col xs={12} md={6} xl={5}>
             <TrackVisibility>
               {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                // <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                <div className={isVisible ? "" : ""}>
                   <img src={headerImg} alt="Header Img"/>
                 </div>}
             </TrackVisibility>
